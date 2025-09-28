9972f757c45dc4bfc96ad5c6030a3b5588795c18
MH020 <166133291+MH020@users.noreply.github.com>
2025-09-28 11:52:55 +0200

    i hate design

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,12 @@ import dom from './domElements.js'
 const navbar = document.getElementById("navbar");
 const page = document.getElementById("page");
 
-navbar.appendChild(dom.navbar())
+
+const welcomeTitle = document.createElement("h1")
+welcomeTitle.textContent = "Bienvenido a mi documentación sobre Node"
+
+navbar.appendChild(dom.navbar());
+page.appendChild(welcomeTitle);
 page.appendChild(dom.textBox("Title text","hola me amigos"))
 page.appendChild(dom.textBox("2end title","more text new"))
 
